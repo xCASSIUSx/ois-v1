5ba9c796bd9e8bac749673fdc46ee71b1944a315
xCASSIUSx <oliver.busan@gmail.com>
2023-02-26 22:16:00 +0100

    Dodaj izračun pospeška

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
 public class Gravitacija {
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        double visina = sc.nextDouble();
+        double pospesek = izracun(visina);
         System.out.print("OIS je zakon!");
+    }
+    public static double izracun (double visina){
+        double C = Double.parseDouble("6.674E-11");
+        double M = Double.parseDouble("5.972E24");
+        double r = Double.parseDouble("6.371E6");
+        double pospesek = (C * M)/((r + visina) * (r * visina));
+        return pospesek;
     }
 }
